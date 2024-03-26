202f0c14805e3286c5f8bb1ef7a905b46503e71f
joelcitorueda <ruedafloresjulian722@gmail.com>
2024-03-26 15:53:19 -0400

    Onichan

--- SistemaVenta/SistemaVenta.Vista/ClienteVistas/ClienteInsertarVista.Designer.cs
+++ SistemaVenta/SistemaVenta.Vista/ClienteVistas/ClienteInsertarVista.Designer.cs
@@ -109,6 +109,7 @@
 			// 
 			// button1
 			// 
+			button1.DialogResult = DialogResult.OK;
 			button1.Location = new Point(204, 313);
 			button1.Name = "button1";
 			button1.Size = new Size(94, 29);
